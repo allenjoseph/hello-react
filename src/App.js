2c789e8bef07bf11717965cc93b00cf7729e57ba
Allen Joseph <allen.joseph.v.a@gmail.com>
2021-08-26 18:17:47 -0500

    feat(tools): add npm ttools for transpile react project and other dev tools

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-const MyComponent = (props) => {
-    return React.createElement('div', {}, [
-        React.createElement('h2', {}, props.name),
-        React.createElement('small', {}, props.description)
-    ]);
-};
+import React from "react";
+import ReactDOM from "react-dom";
+import MyComponent from "./MyComponent";
 
 const App = () => {
-    return React.createElement('div', {}, [
-        React.createElement('h1', {id: 'tag-h1'}, 'Header h1'),
-        React.createElement(MyComponent, {name: 'name 1', description: 'description 1'}),
-        React.createElement(MyComponent, {name: 'name 2', description: 'description 2'}),
-        React.createElement(MyComponent, {name: 'name 3', description: 'description 3'})
-    ]);
+  return React.createElement("div", {}, [
+    React.createElement("h1", { id: "tag-h1" }, "Header h1"),
+    React.createElement(MyComponent, {
+      name: "name 1",
+      description: "description 1",
+    }),
+    React.createElement(MyComponent, {
+      name: "name 2",
+      description: "description 2",
+    }),
+    React.createElement(MyComponent, {
+      name: "name 3",
+      description: "description 3",
+    }),
+  ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
