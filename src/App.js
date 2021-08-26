334c41f89fc7e7b14a43ce598d1ee4bda7a75310
Allen Joseph <allen.joseph.v.a@gmail.com>
2021-08-26 18:34:38 -0500

    feat(jsx): transform vanilla react to jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import React from "react";
 import ReactDOM from "react-dom";
-import MyComponent from "./MyComponent";
+import Animal from "./Animal";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "tag-h1" }, "Header h1"),
-    React.createElement(MyComponent, {
-      name: "name 1",
-      description: "description 1",
-    }),
-    React.createElement(MyComponent, {
-      name: "name 2",
-      description: "description 2",
-    }),
-    React.createElement(MyComponent, {
-      name: "name 3",
-      description: "description 3",
-    }),
-  ]);
-};
+  return (
+    <div>
+      <h1>Zoo</h1>
+      <Animal name="Fox" family="Canidae"></Animal>
+      <Animal name="Bear" family="Ursidae"></Animal>
+    </div>
+  );
+}
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
